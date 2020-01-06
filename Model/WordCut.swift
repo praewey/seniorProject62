6756c455737e9c4880815d26ca5adf81ce2844d3
seniorProject62 <praewz_crazy@hotmail.co.th>
2020-01-06 23:11:54 +0700

    Create Model - Create WordCut.swift

---
+++ Model/WordCut.swift
@@ -0,0 +1,15 @@
+//
+//  WordCut.swift
+//  seniorProject62
+//
+//  Created by Praewey Spokkokkak on 6/1/2563 BE.
+//  Copyright © 2563 Praewey Spokkokkak. All rights reserved.
+//
+
+struct WordCut: Codable {
+    let cut: [String]
+    
+    init() {
+        cut = []
+    }
+}
