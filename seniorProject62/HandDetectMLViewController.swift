ad8a57c9b6258a0e450c6abb5e33cb2dee974b92
seniorProject62 <praewz_crazy@hotmail.co.th>
2020-01-11 21:33:58 +0700

    Create HandDetectMLViewController.swift

---
+++ seniorProject62/HandDetectMLViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HandDetectMLViewController.swift
+//  seniorProject62
+//
+//  Created by Praewey Spokkokkak on 10/1/2563 BE.
+//  Copyright © 2563 Praewey Spokkokkak. All rights reserved.
+//
+
+import UIKit
+
+class HandDetectMLViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
