f846435fa94e5d31f88af6344f6376ad1757ed73
seniorProject62 <praewz_crazy@hotmail.co.th>
2020-01-19 19:18:34 +0700

    Create HandToThai

---
+++ Model/HandToThai.swift
@@ -0,0 +1,28 @@
+//
+//  HandToThai.swift
+//  seniorProject62
+//
+//  Created by Praewey Spokkokkak on 19/1/2563 BE.
+//  Copyright © 2563 Praewey Spokkokkak. All rights reserved.
+//
+
+import Foundation
+
+struct HandToThai {
+    static public let mapC: [String: String] = [
+        "K1": "ข",
+        "K2": "ค",
+        "K3": "ง",
+        "L2": "ล",
+        "L3": "ย"
+    ]
+    
+    static public let mapU: [String: String] = [
+        "U1": "ุ"
+    ]
+    
+    static public let mapT: [String: String] = [
+        "T1": "้"
+    ]
+    
+}
