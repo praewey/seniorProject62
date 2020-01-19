138ee769bcd630be2fa5a36fab6368e5fd60c51a
seniorProject62 <praewz_crazy@hotmail.co.th>
2020-01-19 19:25:16 +0700

    Update HandDetectMLViewController.swft - CameraTapped ปุ่มสลับกล้อง - touchDelete ปุ่มลบ - testChanged เทสคำว่าเปลี่ยนไปตามที่ต้องการไหม - translate ดึงข้อมูลจาก HandToThai มาแสดง

--- seniorProject62/HandDetectMLViewController.swift
+++ seniorProject62/HandDetectMLViewController.swift
@@ -16,6 +16,13 @@ class HandDetectMLViewController: UIViewController, AVCaptureVideoDataOutputSamp
     @IBOutlet weak var detectView: UIView!
     @IBOutlet weak var textTranslateLabel: UILabel!
     
+    @IBOutlet weak var testTextField: UITextField!
+    
+    var translateText: String = ""
+    var timer: Timer?
+    var block: Bool = false
+    var isBackCamera: Bool = true
+    var session: AVCaptureSession!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +30,62 @@ class HandDetectMLViewController: UIViewController, AVCaptureVideoDataOutputSamp
     }
     
     @IBAction func CameraTapped(_ sender: Any) {
-
+        session.beginConfiguration()
+        
+        if isBackCamera {
+            let currentInput = session.inputs.first!
+            session.removeInput(currentInput)
+            
+            let deviceSession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .unspecified)
+            for device in deviceSession.devices {
+                if device.position == .front {
+                    guard let input = try? AVCaptureDeviceInput(device: device) else { return }
+                    
+                    session.addInput(input)
+                    
+                    break
+                }
+            }
+        }
+        session.commitConfiguration()
+    }
+    
+    @IBAction func touchDelete(_ sender: Any) {
+        testTextField.text = ""
+        textTranslateLabel.text = ""
+        translateText = ""
+    }
+    
+    @IBAction func testChanged(_ sender: Any) {
+        textTranslateLabel.text = translate(input: testTextField.text ?? "")
+    }
+    
+    func translate(input: String) -> String {
+        var output = input
+        for text in HandToThai.mapC.keys {
+            output = output.uppercased().replacingOccurrences(of: text, with: HandToThai.mapC[text]!)
+        }
+        for text in HandToThai.mapU.keys {
+            output = output.uppercased().replacingOccurrences(of: text, with: HandToThai.mapU[text]!)
+        }
+        for text in HandToThai.mapT.keys {
+            output = output.uppercased().replacingOccurrences(of: text, with: HandToThai.mapT[text]!)
+        }
+        
+        return output
     }
     
     func setupSession() {
         guard let device = AVCaptureDevice.default(for: .video) else { return }
         guard let input = try? AVCaptureDeviceInput(device: device) else { return }
         
-        let session = AVCaptureSession()
-        session.sessionPreset = .hd4K3840x2160
+        session = AVCaptureSession()
+//        session.sessionPreset = .hd4K3840x2160
+        session.sessionPreset = .hd1920x1080
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer.frame = view.frame
-        detectView.layer.addSublayer(previewLayer)
+        detectView.layer.insertSublayer(previewLayer, at: 0)
         
         let output = AVCaptureVideoDataOutput()
         output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
@@ -45,9 +95,12 @@ class HandDetectMLViewController: UIViewController, AVCaptureVideoDataOutputSamp
         session.addInput(input)
         session.addOutput(output)
         
-        
         // Starts the capture session
         session.startRunning()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+            self.block = false
+        })
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -63,67 +116,74 @@ class HandDetectMLViewController: UIViewController, AVCaptureVideoDataOutputSamp
             guard let mostConfidenceResult = results.first else { return }
             
             DispatchQueue.main.sync {
-                if mostConfidenceResult.confidence >= 0.99 {
+                if mostConfidenceResult.confidence >= 0.80 {
                     let confidenceText = "\n\(Int(mostConfidenceResult.confidence * 100.0))% confidence"
                     
-                    switch mostConfidenceResult.identifier {
-                    case "A":
-                        self.textTranslateLabel.text = "A \(confidenceText)"
-                    case "B":
-                        self.textTranslateLabel.text = "B \(confidenceText)"
-                    case "C":
-                        self.textTranslateLabel.text = "C \(confidenceText)"
-                    case "D":
-                        self.textTranslateLabel.text = "D \(confidenceText)"
-                    case "E":
-                        self.textTranslateLabel.text = "E \(confidenceText)"
-                    case "F":
-                        self.textTranslateLabel.text = "F \(confidenceText)"
-                    case "G":
-                        self.textTranslateLabel.text = "G \(confidenceText)"
-                    case "H":
-                        self.textTranslateLabel.text = "H \(confidenceText)"
-                    case "I":
-                        self.textTranslateLabel.text = "I \(confidenceText)"
-                    case "J":
-                        self.textTranslateLabel.text = "J \(confidenceText)"
-                    case "K":
-                        self.textTranslateLabel.text = "K \(confidenceText)"
-                    case "L":
-                        self.textTranslateLabel.text = "L \(confidenceText)"
-                    case "M":
-                        self.textTranslateLabel.text = "M \(confidenceText)"
-                    case "N":
-                        self.textTranslateLabel.text = "N \(confidenceText)"
-                    case "O":
-                        self.textTranslateLabel.text = "O \(confidenceText)"
-                    case "P":
-                        self.textTranslateLabel.text = "P \(confidenceText)"
-                    case "Q":
-                        self.textTranslateLabel.text = "Q \(confidenceText)"
-                    case "R":
-                        self.textTranslateLabel.text = "R \(confidenceText)"
-                    case "S":
-                        self.textTranslateLabel.text = "S \(confidenceText)"
-                    case "T":
-                        self.textTranslateLabel.text = "T \(confidenceText)"
-                    case "U":
-                        self.textTranslateLabel.text = "U \(confidenceText)"
-                    case "V":
-                        self.textTranslateLabel.text = "V \(confidenceText)"
-                    case "W":
-                        self.textTranslateLabel.text = "W \(confidenceText)"
-                    case "X":
-                        self.textTranslateLabel.text = "X \(confidenceText)"
-                    case "Y":
-                        self.textTranslateLabel.text = "Y \(confidenceText)"
-                    case "Z":
-                        self.textTranslateLabel.text = "Z \(confidenceText)"
-                    default:
-                        return
+                    if mostConfidenceResult.identifier != "nothing" && !self.block {
+                        self.translateText += mostConfidenceResult.identifier
+                        self.textTranslateLabel.text = self.translate(input: self.translateText)
+                        
+                        self.block = true
                     }
+                    
+                    //                    switch mostConfidenceResult.identifier {
+                    //                    case "A":
+                    //                        self.translateText = "A \(confidenceText)"
+                    //                    case "B":
+                    //                        self.translateText = "B \(confidenceText)"
+                    //                    case "C":
+                    //                        self.translateText = "C \(confidenceText)"
+                    //                    case "D":
+                    //                        self.translateText = "D \(confidenceText)"
+                    //                    case "E":
+                    //                        self.translateText = "E \(confidenceText)"
+                    //                    case "F":
+                    //                        self.translateText = "F \(confidenceText)"
+                    //                    case "G":
+                    //                        self.translateText = "G \(confidenceText)"
+                    //                    case "H":
+                    //                        self.translateText = "H \(confidenceText)"
+                    //                    case "I":
+                    //                        self.translateText = "I \(confidenceText)"
+                    //                    case "J":
+                    //                        self.translateText = "J \(confidenceText)"
+                    //                    case "K":
+                    //                        self.translateText = "K \(confidenceText)"
+                    //                    case "L":
+                    //                        self.translateText = "L \(confidenceText)"
+                    //                    case "M":
+                    //                        self.translateText = "M \(confidenceText)"
+                    //                    case "N":
+                    //                        self.translateText = "N \(confidenceText)"
+                    //                    case "O":
+                    //                        self.translateText = "O \(confidenceText)"
+                    //                    case "P":
+                    //                        self.translateText = "P \(confidenceText)"
+                    //                    case "Q":
+                    //                        self.translateText = "Q \(confidenceText)"
+                    //                    case "R":
+                    //                        self.translateText = "R \(confidenceText)"
+                    //                    case "S":
+                    //                        self.translateText = "S \(confidenceText)"
+                    //                    case "T":
+                    //                        self.translateText = "T \(confidenceText)"
+                    //                    case "U":
+                    //                        self.translateText = "U \(confidenceText)"
+                    //                    case "V":
+                    //                        self.translateText = "V \(confidenceText)"
+                    //                    case "W":
+                    //                        self.translateText = "W \(confidenceText)"
+                    //                    case "X":
+                    //                        self.translateText = "X \(confidenceText)"
+                    //                    case "Y":
+                    //                        self.translateText = "Y \(confidenceText)"
+                    //                    case "Z":
+                    //                        self.translateText = "Z \(confidenceText)"
+                    //                    default:
+                    //                        return
+                    //                    }
                 } else {
-                    self.textTranslateLabel.text = "กำลังประมวลผล!"
+//                    self.textTranslateLabel.text = "กำลังประมวลผล!"
                 }
             }
         }
